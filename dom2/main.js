226ca90e77910647e009ef228b3d84a3f7fd9e47
DebbieZimmerman <debbieseuss@gmail.com>
2020-11-02 18:40:08 +0200

    initial code

---
+++ dom2/main.js
@@ -0,0 +1,14 @@
+
+const validate = function() {
+    let name = document.getElementById("name").value
+    let salary = document.getElementById("salary").value
+    let birthday = document.getElementById("birthday").value
+    let phone = document.getElementById("phone").value
+    if (name.length <=2){
+        let notification = document.createElement("span")
+        notification
+        notification.innerHTML = "Name must be at least 3 characters long"
+        notification.
+    } 
+    console.log("Thank you for submitting your information")
+}
